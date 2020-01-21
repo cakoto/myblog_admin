cb243c9d5f0211bfcd50607a1729447ae6447515
durarara <42024815+cakoto@users.noreply.github.com>
2020-01-21 17:08:39 +0800

    创建项目

---
+++ src/main/java/com/gyf/myblog_admin/MyblogAdminApplication.java
@@ -0,0 +1,13 @@
+package com.gyf.myblog_admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyblogAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyblogAdminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gyf/myblog_admin/MyblogAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gyf.myblog_admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyblogAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
