d68dfd4fed3ee444f8f5d4edd8e01c2d4ef6a6b6
durarara <42024815+cakoto@users.noreply.github.com>
2020-03-31 21:27:43 +0800

    日志管理(切面)

--- src/main/java/com/gyf/myblog_admin/web/IndexController.java
+++ src/main/java/com/gyf/myblog_admin/web/IndexController.java
@@ -3,22 +3,24 @@ package com.gyf.myblog_admin.web;
 import com.gyf.myblog_admin.NotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class IndexController {
 
-    @GetMapping("/")
-    public String index(){
+    @GetMapping("/{id}/{name}")
+    public String index(@PathVariable Integer id,@PathVariable String name){
 
         /*
          * 控制返回一个页面+后台控制信息
          */
         //int i = 9/0;
-        String blog = null;
-        if(blog == null){
-            //return 404;不要这样做
-            throw new NotFoundException("博客不存在");
-        }
+//        String blog = null;
+//        if(blog == null){
+//            //return 404;不要这样做
+//            throw new NotFoundException("博客不存在");
+//        }
+        System.out.println("-------index------");
         return "index";
     }
 
